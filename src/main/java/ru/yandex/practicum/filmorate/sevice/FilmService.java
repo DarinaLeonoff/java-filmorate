3c92c6d83625d5277cbaa851072161045c4d4c68
Daria Leo <dashunkaleo@yandex.ru>
2025-08-15 16:01:38 +0300

    prepare services

---
+++ src/main/java/ru/yandex/practicum/filmorate/sevice/FilmService.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.sevice;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+
+@Service
+public class FilmService {
+//    public void addLike(Film film){}
+//    public void deleteLike(Film film){}
+//    public Collection<Film> getTopTen(){}
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/sevice/UserService.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.sevice;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+
+@Service
+public class UserService {
+//    public void addFriend(User user){}
+//    public void deleteFriend(User user){}
+//    public Collection<User> mutualFriends(User user){}
+}
